34b3649f88980b4a6dea96e9ee9071fb3b2c52d5
Shobhit977 <shobhit00gupta@gmail.com>
2023-12-09 13:00:23 +0530

    feat : function to get mock data

---
+++ api/mockService/getMockData/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/mta-hosting-optimizer/lib/constants"
+	s3helper "github.com/mta-hosting-optimizer/lib/s3Helper"
+	"github.com/mta-hosting-optimizer/lib/service"
+)
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	svc, err := service.NewService()
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			Body: err.Error(),
+		}, nil
+	}
+	ipConfig, err := getIpConfig(svc, req)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{}, err
+	}
+	return events.APIGatewayV2HTTPResponse{StatusCode: 200, Body: string(ipConfig)}, nil
+}
+func getIpConfig(svc service.Service, req events.APIGatewayV2HTTPRequest) ([]byte, error) {
+	if !isFileExist(svc) {
+		return nil, errors.New("ip configuration data not found")
+	}
+	ipConfig, err := s3helper.GetS3Object(svc, constants.Bucket, constants.Key)
+	if err != nil {
+		log.Printf("%v", err)
+		return nil, err
+	}
+	return ipConfig, nil
+}
+
+func isFileExist(svc service.Service) bool {
+	exist, err := s3helper.KeyExists(svc, constants.Bucket, constants.Key)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	return exist
+}
+func main() {
+	lambda.Start(handler)
+}
